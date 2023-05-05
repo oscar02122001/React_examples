a8ca28eff4111d7471c8d32d0047721fe935d6e7
oscar02122001 <olimjonismatov01@gmail.com>
2023-05-05 11:53:47 +0500

    router_6 navbar

---
+++ src/components/routerDom5/navbar.jsx
@@ -0,0 +1,34 @@
+
+
+// export const navbar = [
+//   {
+//     id: 1,
+//     name: "Home",
+//     path: "/home",
+//     element: <Home />,
+//   },
+//   {
+//     id: 2,
+//     name: "About",
+//     path: "/about",
+//     element: <About />,
+//   },
+//   {
+//     id: 3,
+//     name: "Services",
+//     path: "/services",
+//     element: <Services />,
+//   },
+//   {
+//     id: 4,
+//     name: "Location",
+//     path: "/location",
+//     element: <Location />,
+//   },
+//   {
+//     id: 5,
+//     name: "Contact",
+//     path: "/contact",
+//     element: <Contact />,
+//   },
+// ];

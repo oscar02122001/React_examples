123998fc2e30a63b4019a497317438081929398a
oscar02122001 <olimjonismatov01@gmail.com>
2023-05-23 06:29:16 +0500

    fetch

--- src/components/fetchAPI/index.jsx
+++ src/components/fetchAPI/index.jsx
@@ -2,26 +2,43 @@ import React, { useEffect, useRef, useState } from "react";
 
 const Fetch = () => {
   const [data, setData] = useState([]);
+  const [apiData, setApi] = useState("");
+  const [check, setCheck] = useState(false);
   const input = useRef("");
 
-  const renderCountry = () => {
-    // useEffect(() => {
-    fetch(`https://restcountries.com/v3.1/name/${input.current.value}`)
-      .then((res) => res.json())
-      .then((res) => setData(res[0]));
-    // }, []);
-  };
-  // console.log(data.name?.common);
-  console.log(input);
+
+  useEffect(() => {
+    check &&
+      fetch(`https://restcountries.com/v3.1/name/${apiData}`)
+        .then((res) => res.json())
+        .then((res) => setData(res[0]));
+  }, [apiData]);
+
   return (
     <div>
-      <p className="titles">Fetch API </p>
-      <input rel={input} type="text" />
-      <button onClick={renderCountry}>Click</button>
-      <h1>{data.name?.common}</h1>
-      {data.borders?.map((v) => {
-        return <h1> {v}</h1>;
+      <p className="titles">Fetch API</p>
+      <input ref={input} type="text" />
+      <button
+        onClick={() => {
+          setCheck(true);
+          setApi(input.current.value);
+        }}
+      >
+        Click
+      </button>
+      <h1>
+        {data?.name?.common} -{check && (data?.population / 1000000).toFixed(1)}{" "}
+        bln
+      </h1>
+      <h1>Borders:</h1>
+      {data?.borders?.map((v) => {
+        return (
+          <h1 style={{ display: "flex" }} key={v.id}>
+            {v}
+          </h1>
+        );
       })}
+      
     </div>
   );
 };
